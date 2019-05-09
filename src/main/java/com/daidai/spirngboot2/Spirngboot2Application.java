2b27bbe416d0f0e1433925dc4a103b7e00fd9cdc
daidai <244088167@qq.com>
2019-05-09 18:08:55 +0800

    地刺一次

---
+++ src/main/java/com/daidai/spirngboot2/Spirngboot2Application.java
@@ -0,0 +1,13 @@
+package com.daidai.spirngboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spirngboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spirngboot2Application.class, args);
+    }
+
+}
